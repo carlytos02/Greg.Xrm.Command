d7c9c05e60169998245cb74dccbf9b8e7d929f30
c.solimando <c.solimando@avanade.com>
2024-03-16 13:18:06 +0100

    starting second command

---
+++ Greg.Xrm.Command.Core/Commands/AppModule/ListAppRolesCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace Greg.Xrm.Command.Commands.AppModule
+{
+    [Command("appmodule", "listroles", HelpText = "Lists the roles associated to one or all model-driven apps.")]
+    public class ListAppRolesCommand
+    {
+        [Option("all", "a", HelpText = "Show all model-driven app / roles associations", DefaultValue = false)]
+        public bool All { get; set; } = false;
+
+        [Option("id", HelpText = "Model-driven app id used to retrieve related roles", DefaultValue = false)]
+        public string? ModelDrivenAppId { get; set; }
+    }
+}
---
+++ Greg.Xrm.Command.Core/Commands/AppModule/ListAppRolesExecutor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greg.Xrm.Command.Commands.AppModule
+{
+    internal class ListAppRolesExecutor
+    {
+    }
+}
